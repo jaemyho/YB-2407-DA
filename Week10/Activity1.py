a4d82532e93b03569f6c33c92ee68ac89d93f047
jaemyho <jaemy.ho@gmail.com>
2025-02-12 16:05:22 +1300

    output plot

--- Week10/Activity1.py
+++ Week10/Activity1.py
@@ -5,4 +5,7 @@ import pandas as pd
 df = pd.read_csv("./dataset/Sample_Data_for_Activity.csv")
 print(df.head())
 
-print(sns.displot(df['Normal_Distribution'], kde=True))
+sns.displot(df['Normal_Distribution'], kde=True)
+
+output_path = "./output/Activity1Plot.jpg"
+plt.savefig(output_path)
